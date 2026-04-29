9013f59ff666ccf54c3d8b8a65927725a651d22b
loicj <loic.jacquel@gmail.com>
2026-04-29 00:11:39 +0000

    add devcontainer

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from '@playwright/test'
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   timeout: 15_000,
   use: {
-    baseURL: 'http://localhost:5173',
+    baseURL: "http://localhost:5173",
     viewport: { width: 480, height: 854 },
     launchOptions: {
-      executablePath: '/home/loic/.cache/ms-playwright/chromium-1217/chrome-linux64/chrome',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: process.env["PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH"],
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--use-gl=swiftshader",
+        "--enable-webgl",
+      ],
     },
   },
   webServer: {
-    command: 'pnpm dev --port 5173',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env['CI'],
+    command: "pnpm dev --port 5173",
+    url: "http://localhost:5173",
+    reuseExistingServer: !process.env["CI"],
   },
-})
+});
